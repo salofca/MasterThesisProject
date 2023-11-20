187bc581661723438068bf5e031ce9249fdc4f52
salofca <gf.cavaco@campus.fct.unl.pt>
2023-11-20 22:33:04 +0000

    added estimation

--- PartitionMeanBased.py
+++ PartitionMeanBased.py
@@ -1,44 +1,47 @@
+import time
+
 import numpy as np
 from matplotlib import pyplot as plt
-
+import random
 
 PACE = 0.50
 C = 2
-THRESHOLD = 100
 MIN_DIFF = 0.7
 ERROR = 20
 
 
 
 
-def mean_based(n, T, mutation=False):
+def mean_based(x,n, T, mutation=False):
     x_pred = []
     traces = []
     start = 0
     end = n // 2
     remaining_traces = 0
     epsilon = 0
+    threshold = 128
 
     while (end < n) or (start != n):
-        traces = list(traces)
-        for i in range(T - remaining_traces):  # Trace generation
-            k = np.random.randint(n+1)
-            if k != 0:
-                trace = x[:-k].copy()
-                if mutation:
-                    epsilon = np.random.uniform(0,1)
-                    for ix,ti in enumerate(trace):
-                        if epsilon >= 0.5:
-                            trace[ix] = 1-ti
-                suffix = np.random.randint(2, size=k)
-                trace = np.concatenate((trace, suffix), axis=None)
-                traces.append(trace)
-            else:
-                traces.append(x.copy())
+        k_values = np.random.randint(n + 1, size=T-remaining_traces)
 
-        traces = np.vstack(traces)  # stacking the traces all together
+        traces = np.zeros((T, n), dtype=int)
 
-        if n - start <= THRESHOLD:
+
+        for i, k in enumerate(k_values):
+            traces[i, :n - k] = x[:n-k]
+            if mutation:
+                epsilon = np.random.uniform(0, 1, size=len(traces))
+                traces = 1 - traces * (epsilon >= 0.5)
+            traces[i, n - k:] = np.random.randint(2, size=k)
+
+
+
+
+
+
+          # stacking the traces all together
+
+        if n - start <= threshold:
             end = n
 
         mean_y_x = np.mean(traces[:, start:end], axis=0)
@@ -50,7 +53,7 @@ def mean_based(n, T, mutation=False):
         if not mutation:
             for i in range(traces.shape[0]):
                 t = traces[i, :end]
-                for (xi, ti) in zip(x[:end], t):
+                for (xi, ti) in zip(x_pred[:end], t):
                     if xi != ti:
                         rows_delete.append(i)
                         break
@@ -66,8 +69,10 @@ def mean_based(n, T, mutation=False):
 
         traces = np.delete(traces, rows_delete, 0)
         remaining_traces = traces.shape[0]
+        seen = end - start
         start = end
         end = min(end + int(PACE * end), n)
+        threshold = int(threshold + seen*np.sqrt(seen))
     errors = np.mean(x != np.array(x_pred))
     return errors
 
@@ -79,13 +84,13 @@ if __name__ == '__main__':
     target_y = []
     target_x = []
 
-    for n in range(128, 512, 64):  # Applying mean-based over a sequence of n
+    for n in range(256, 1024, 128):  # Applying mean-based over a sequence of n
         corrects = 0
         error = 0
         for rep in range(1, 500):
-            x = np.random.randint(2, size=n)
+            x = [random.choice([0, 1]) for _ in range(n)]
             T = round(10 * n * np.log2(n))
-            result = mean_based(n, T,True)
+            result = mean_based(x,n, T,False)
             error += result
             print(f"rep: {rep}")
         error = error / 500
@@ -93,7 +98,7 @@ if __name__ == '__main__':
         avg_error_n.append(error)
         print(f"{n} error: {error}")
 
-    for i in range(64, 512):
+    for i in range(256, 1025):
         target_x.append(i)
         target_y.append(1 / i)
 
